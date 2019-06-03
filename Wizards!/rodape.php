cb54e920ca1101eb6cb156180713792009ee9dc8
cesaramorim <33498994+cesaramorim@users.noreply.github.com>
2019-06-03 19:19:36 -0300

    Add files via upload

--- Wizards!/rodape.php
+++ Wizards!/rodape.php
@@ -1,3 +1,12 @@
 <footer class="w3-container w3-theme-d5">
-  <p>Magicamente fabricado por:&nbsp&nbsp&nbsp&nbsp César Amorim &nbsp&nbsp&nbsp&nbsp<i class="fas fa-hat-wizard"></i>&nbsp&nbsp&nbsp&nbsp Daniel Leal &nbsp&nbsp&nbsp&nbsp<i class="fas fa-hat-wizard"></i>&nbsp&nbsp&nbsp&nbsp Jamile Oliveira &nbsp&nbsp&nbsp&nbsp</p>
+  <p>Magicamente fabricado por:&nbsp&nbsp&nbsp&nbsp 
+
+  	
+
+  	<a href="http://andrecosta.info/unifacs/gamifica/usuarios/foto_perfil/569_20190211164243.jpeg" style="text-decoration:none;" target="_blank">César Amorim</a> &nbsp&nbsp&nbsp&nbsp 
+
+  	<i class="fas fa-hat-wizard"></i>&nbsp&nbsp&nbsp&nbsp <a href="http://andrecosta.info/unifacs/gamifica/usuarios/foto_perfil/577_20190214053702.jpeg" style="text-decoration:none;" target="_blank">Daniel Leal</a> &nbsp&nbsp&nbsp&nbsp
+
+  	<i class="fas fa-hat-wizard"></i>&nbsp&nbsp&nbsp&nbsp <a href="http://andrecosta.info/unifacs/gamifica/usuarios/foto_perfil/567_20190216170203.jpeg" style="text-decoration:none;" target="_blank">Jamile Oliveira</a> &nbsp&nbsp&nbsp&nbsp</p>
+
 </footer>
